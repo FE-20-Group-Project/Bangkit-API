32a9391745f3226a26d8750f52204e426d598a35
fawwaz37 <fawwazarfiqi@gmail.com>
2022-11-23 21:40:35 +0700

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import passport from "passport";
 import morgan from "morgan";
+import cors from "cors";
 
 import Mongo from "./database/connect/mongo.connect.js";
 import { Redis } from "./database/connect/redis.connect.js";
@@ -16,6 +17,18 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan(`[LOG] ipAddr=:remote-addr date=[:date[web]] time=:response-time ms method=:method url=":url" status=":status" `));
+
+const corsConfig = {
+	// origin: true,
+	credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 app.use(passport.initialize());
 passp(passport);
