9ea66bdc644ce32f8a37ee100ec0ce6154a7be1a
fawwaz37 <fawwazarfiqi@gmail.com>
2022-11-25 19:47:24 +0700

    Add image user n instansi, + edit user n instansi

--- database/db/instansi.db.js
+++ database/db/instansi.db.js
@@ -5,8 +5,8 @@ export default class InstansiDB {
 		this.instansi = Instansi;
 	}
 
-	async createInstansi(name, email, password) {
-		const data = await this.instansi.create({ name, email, password });
+	async createInstansi(name, email, password, image) {
+		const data = await this.instansi.create({ name, email, password, image });
 		return data;
 	}
 
@@ -14,5 +14,16 @@ export default class InstansiDB {
 		const data = await this.instansi.findOne({ email });
 		const result = data ? data : null;
 		return result;
+	}
+
+	async findInstansiById(_id) {
+		const data = await this.instansi.findOne({ _id });
+		const result = data ? data : null;
+		return result;
+	}
+
+	async updateInstansiData(_id, password, name, email) {
+		const datas = await this.instansi.findOneAndUpdate({ _id }, { password, name, email }, { new: true });
+		return datas;
 	}
 }
--- database/models/instansi.model.js
+++ database/models/instansi.model.js
@@ -16,6 +16,10 @@ const InstansiSchema = new mongoose.Schema(
 			required: true,
 			minlength: 6,
 		},
+		image: {
+			type: String,
+			required: true,
+		},
 	},
 	{ versionKey: false }
 );
